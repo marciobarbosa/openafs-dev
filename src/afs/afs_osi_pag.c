9cafc7f3a6d7c72f8ad2435430d4b128da8301e9
Marcio Barbosa <mbarbosa@sinenomine.net>
2016-04-22 17:31:58 -0300

    afs: afs_pag_wait() makes process unkillable
    The afs_pag_wait() function is called every time we need to
    create a new pag. If the caller is not a super user, this
    function is gonna sleep until the point where the difference
    between the current time and pag_epoch gets greater than the
    pagCounter. Unfortunaly, the function in question ignores
    the code provided by afs_osi_Wait(). As a result, it is not
    possible to kill the process that requested the new pag while
    afs_pag_wait() is still slepping. In fact, if the user tries
    to kill this process it will spin the cpu.
    
    To fix this problem, do not ignore the code provided by the
    function afs_osi_Wait().

--- src/afs/afs_osi_pag.c
+++ src/afs/afs_osi_pag.c
@@ -169,6 +169,8 @@ afs_pag_sleep(afs_ucred_t **acred)
 static int
 afs_pag_wait(afs_ucred_t **acred)
 {
+    int code = 0;
+
     if (afs_pag_sleep(acred)) {
 	if (!afs_pag_sleepcnt) {
 	    afs_warn("%s() PAG throttling triggered, pid %d... sleeping.  sleepcnt %d\n",
@@ -178,14 +180,13 @@ afs_pag_wait(afs_ucred_t **acred)
 	afs_pag_sleepcnt++;
 
 	do {
-	    /* XXX spins on EINTR */
-	    afs_osi_Wait(1000, (struct afs_osi_WaitHandle *)0, 0);
-	} while (afs_pag_sleep(acred));
+	    code = afs_osi_Wait(1000, (struct afs_osi_WaitHandle *)0, 0);
+	} while (afs_pag_sleep(acred) && !code);
 
 	afs_pag_sleepcnt--;
     }
 
-    return 0;
+    return code;
 }
 
 int
@@ -219,7 +220,10 @@ afs_setpag(void)
 
     AFS_STATCNT(afs_setpag);
 
-    afs_pag_wait(acred);
+    code = afs_pag_wait(acred);
+    if (code) {
+	return code;
+    }
 
 
 #if	defined(AFS_SUN5_ENV)
@@ -329,7 +333,10 @@ afs_setpag_val(int pagval)
 
     AFS_STATCNT(afs_setpag);
 
-    afs_pag_wait(acred);
+    code = afs_pag_wait(acred);
+    if (code) {
+	return code;
+    }
 
 #if	defined(AFS_SUN5_ENV)
     code = AddPag(pagval, credpp);
