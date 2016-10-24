a6768eae8a498062d7e61ca38577f891f3906508
Marcio Barbosa <mbarbosa@sinenomine.net>
2016-10-23 19:01:30 -0700

    Working

--- osx.sh
+++ osx.sh
@@ -4,6 +4,6 @@ make clean
 
 ./regen
 
-ARCHFLAGS="-arch i386 -arch x86_64" ./configure --enable-transarc-paths --with-krb5-conf=/usr/bin/krb5-config
+ARCHFLAGS="-arch x86_64" ./configure --enable-transarc-paths --with-krb5-conf=/usr/bin/krb5-config
 
-ARCHFLAGS="-arch i386 -arch x86_64" make dest
+ARCHFLAGS="-arch x86_64" make dest
