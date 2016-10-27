5af9377b1ad94d0c8001f8b0b6a1472542028f6e
Marcio Barbosa <marcio@Marcios-MacBook-Pro-3.local>
2016-10-27 10:26:36 -0700

    wip

--- osx.sh
+++ osx.sh
@@ -4,6 +4,6 @@ make clean
 
 ./regen
 
-ARCHFLAGS="-arch x86_64" ./configure --enable-transarc-paths --with-krb5-conf=/usr/bin/krb5-config
+ARCHFLAGS="-arch x86_64" ./configure --enable-transarc-paths --with-krb5-conf=/usr/bin/krb5-config --enable-debug-kernel
 
 ARCHFLAGS="-arch x86_64" make dest
