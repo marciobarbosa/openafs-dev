4727a95ce40f3a312170e0299a8883270d0fafb6
Marcio Barbosa <mbarbosa@sinenomine.net>
2017-11-16 17:24:03 -0500

    afs: fix kernel_write / kernel_read arguments
    The order / content of the arguments passed to kernel_write and
    kernel_read are not right. As a result, the kernel will panic if one of
    the functions in question is called.
    
    FIXES 134440

--- src/afs/LINUX/osi_compat.h
+++ src/afs/LINUX/osi_compat.h
@@ -659,7 +659,7 @@ afs_file_read(struct file *filp, char __user *buf, size_t len, loff_t *pos)
 #if defined(HAVE_LINUX___VFS_WRITE)
     return __vfs_read(filp, buf, len, pos);
 #elif defined(HAVE_LINUX_KERNEL_WRITE)
-    return kernel_read(filp, buf, len, pos);
+    return kernel_read(filp, *pos, buf, len);
 #else
     return filp->f_op->read(filp, buf, len, pos);
 #endif
@@ -671,7 +671,7 @@ afs_file_write(struct file *filp, char __user *buf, size_t len, loff_t *pos)
 #if defined(HAVE_LINUX___VFS_WRITE)
     return __vfs_write(filp, buf, len, pos);
 #elif defined(HAVE_LINUX_KERNEL_WRITE)
-    return kernel_write(filp, buf, len, pos);
+    return kernel_write(filp, buf, len, *pos);
 #else
     return filp->f_op->write(filp, buf, len, pos);
 #endif
