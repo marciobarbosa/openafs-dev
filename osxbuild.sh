2f67b8166f044a004c3f6c8237d4b4924162c55f
Marcio Barbosa <mbarbosa@sinenomine.net>
2019-09-05 00:51:11 -0300

    osx: script to build the code
    Change-Id: I0fc4df65ef1866eb46f72bf0d34bde63c8bdb3d7

---
+++ osxbuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+make clean
+
+./regen.sh
+
+ARCHFLAGS="-arch x86_64" ./configure --enable-transarc-paths \
+--with-krb5-conf=/usr/bin/krb5-config --enable-debug-kernel --libdir=/Library/OpenAFS/Tools/lib
+
+ARCHFLAGS="-arch x86_64" make dest
