d043f3db9f142a18188eb8a3d2937694eeae17ca
Marcio Barbosa <mbarbosa@sinenomine.net>
2020-03-20 03:09:31 +0000

    vol-info: use opr_queue_Entry to get queue entries
    The opr library provides a set of functions to safely handle opr_queue
    structs. Unfortunately, a few functions from vol-info.c manipulate these
    structs without using the proper function to get entries from it.
    
    To be safe, use opr_queue_Entry to get entries from opr_queue structs.
    
    Change-Id: If0c44e4b61e8f1436363a25d4637d15ff5ee4b01

--- src/vol/vol-info.c
+++ src/vol/vol-info.c
@@ -1705,7 +1705,7 @@ RunVnodeScanProcs(struct VolInfoOpt *opt, VnodeClass class,
     struct VnodeScanProc *entry;
 
     for (opr_queue_Scan(scanList, cursor)) {
-	entry = (struct VnodeScanProc *)cursor;
+	entry = opr_queue_Entry(cursor, struct VnodeScanProc, link);
 	if (entry->proc) {
 	    (*entry->proc) (opt, vnodeDetails);
 	}
@@ -1792,7 +1792,8 @@ HandleVnodes(struct VolInfoOpt *opt, Volume * vp, VnodeClass class)
     }
 
     for (opr_queue_Scan(scanList, cursor)) {
-	struct VnodeScanProc *entry = (struct VnodeScanProc *)cursor;
+	struct VnodeScanProc *entry;
+	entry = opr_queue_Entry(cursor, struct VnodeScanProc, link);
 	if (entry->heading) {
 	    printf("%s", entry->heading);
 	}
