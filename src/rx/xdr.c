44a7b9362e114086e742f9067cf75ac68e1b4396
Marcio Barbosa <mbarbosa@sinenomine.net>
2021-08-05 04:09:10 +0000

    macos: add support for Apple Silicon
    This commit introduces the new set of changes / files required to
    successfully build the OpenAFS source code on Apple M1 (macOS 11+).
    
    Change-Id: Ib005215ae9bfb0b9dda94b2e194f05c0ce92fa1e

--- src/rx/xdr.c
+++ src/rx/xdr.c
@@ -591,7 +591,7 @@ xdr_free(xdrproc_t proc, void *obj)
     x.x_op = XDR_FREE;
 
     /* See note in xdr.h for the method behind this madness */
-#if defined(AFS_I386_LINUX_ENV) && defined(KERNEL) && !defined(UKERNEL)
+#if ((defined(AFS_I386_LINUX_ENV) && defined(KERNEL)) || defined(AFS_ARM64_DARWIN_ENV)) && !defined(UKERNEL)
     (*proc)(&x, obj, 0);
 #else
     (*proc)(&x, obj);
