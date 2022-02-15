e54b049126f0bed9fb9d20bf0a9bd9854378ff1d
Marcio Barbosa <mbarbosa@sinenomine.net>
2022-02-15 04:44:28 +0000

    macos: Add support for Apple Silicon
    This commit introduces the new set of changes / files required to
    successfully build the OpenAFS source code on Apple M1 (macOS 11+).
    
    While here, avoid repetition by moving the arch-agnostic stuff from
    param.x86_darwin_200.h to a new file (param.generic_darwin_200.h) so it
    can be used by other architectures (arm64).
    
    Change-Id: Ib005215ae9bfb0b9dda94b2e194f05c0ce92fa1e

---
+++ src/config/param.arm_darwin_200.h
@@ -0,0 +1,17 @@
+#if defined(__arm64__)
+
+#ifndef AFS_ARM64_PARAM_H
+#define AFS_ARM64_PARAM_H
+
+#define AFS_ARM_ENV		1
+#define AFS_ARM64_DARWIN_ENV	1
+
+#define sys_arm_darwin_200	1
+
+#define SYS_NAME		"arm_darwin_200"
+#define SYS_NAME_ID		SYS_NAME_ID_arm_darwin_200
+#define AFSLITTLE_ENDIAN	1
+
+#endif /* AFS_ARM64_PARAM_H */
+
+#endif /* __arm64__ */
